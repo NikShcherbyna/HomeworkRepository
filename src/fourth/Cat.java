106f957fc87e5d775968aa6a5394ffbfc6b239cb
Nikita Shcherbyna <CherbinaNikita@gmail.com>
2022-08-15 12:38:06 +0300

    hometask 4

--- src/fourth/Cat.java
+++ src/fourth/Cat.java
@@ -15,7 +15,7 @@ public class Cat extends Animal{
     @Override
     public void run(int runDistance) {
         if (runDistance > 0 && runDistance < 200){
-            System.out.println("Cat" + " " + name + " " + " has ran" + runDistance + " " + "metres");
+            System.out.println("Cat" + " " + " " + name + " " + " has ran" + runDistance + " " + "metres");
         }else{
             System.out.println("Your cat" + " " + name + " " + "can run not more than 200 metres and less than 0");
         }
